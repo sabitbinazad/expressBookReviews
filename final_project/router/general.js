36eeeb3252b7ae40f18abad42f0ebbe2eaf58e6b
sabitbinazad <sabitbinazad01@gmail.com>
2024-12-08 13:34:43 -0500

    task 4 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,13 +45,24 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = [];
+
+  for (const isbn in books) {
+      if (books[isbn].title.toLowerCase() === title.toLowerCase()) {
+          matchingBooks.push(books[isbn]);
+      }
+  }
+
+  res.send(matchingBooks);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
